891afa70e12c2bf9d947706f22d129a483b7b4de
whajji1026 <wyeemhajjy26@gmail.com>
2024-10-20 22:52:29 +0100

    hyperparameter tuning added

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 import numpy as np 
 import pandas as pd
 import dill
@@ -21,18 +22,19 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            # para=param[list(models.keys())[i]]
+            print(f"model : {model}")
+            para=param[list(models.keys())[i]]
 
-            # gs = GridSearchCV(model,para,cv=3)
-            # gs.fit(X_train,y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            # model.set_params(**gs.best_params_)
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
@@ -52,7 +54,6 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
     except Exception as e:
         raise CustomException(e, sys)
     
-
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
